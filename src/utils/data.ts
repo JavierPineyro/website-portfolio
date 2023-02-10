71262713bb81f023198da054f0ce946e397c2e72
JavierPineyro <javidunam22@gmail.com>
2023-02-10 19:44:06 -0300

    Rename data files

---
+++ src/utils/data.ts
@@ -0,0 +1,49 @@
+export const Links = [
+  {
+    label: 'Inicio',
+    url: '#top',
+  },
+  {
+    label: 'Proyectos',
+    url: '#proyectos',
+  },
+  {
+    label: 'Tecnologías',
+    url: '#tecnologias',
+  },
+  {
+    label: 'Sobre Mi',
+    url: '#sobremi',
+  },
+]
+
+export const Projects = [
+  {
+    title: "Juego Tic-Tac-Toe",
+    description: "Un mini juego construido con React",
+    url: "https://tic-tac-toe-vite-javierpineyro.vercel.app/",
+    repo: "https://github.com/JavierPineyro/tic-tac-toe-vite",
+    stack: ["React", "Tailwind", "Vite"]
+  },
+  {
+    title: "Search Country App",
+    description: "Web app para buscar información sobre países",
+    url: "https://search-country-javierpineyro.vercel.app/",
+    repo: "https://github.com/JavierPineyro/next-country",
+    stack: ["NextJs", "React", "Typescript"]
+  },
+  {
+    title: "Advency challenge",
+    description: "Code challenge de navidad de GoncyPozzo",
+    url: "https://search-country-javierpineyro.vercel.app/",
+    repo: "https://github.com/JavierPineyro/advency-challenge",
+    stack: ["React", "ChakraUI", "Framer Motion"]
+  },
+  {
+    title: "TODO app",
+    description: "Aplicación Web TO-DO para anotar tareas",
+    url: "https://search-country-javierpineyro.vercel.app/",
+    repo: "https://github.com/JavierPineyro/advency-challenge",
+    stack: ["React", "ChakraUI", "Vite"]
+  }
+]
