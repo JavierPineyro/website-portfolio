33cbe368f1e149b98280b5e1c00c18c5554e168c
JavierPineyro <javidunam22@gmail.com>
2023-02-09 22:59:35 -0300

    Refactor move constants to its own module

---
+++ src/utils/constants.ts
@@ -0,0 +1,26 @@
+export const Links = [
+  {
+    label: 'Inicio',
+    url: '#top',
+  },
+  {
+    label: 'Proyectos',
+    url: '#proyectos',
+  },
+  {
+    label: 'Tecnologías',
+    url: '#tecnologias',
+  },
+  {
+    label: 'Sobre Mi',
+    url: '#sobremi',
+  },
+]
+
+export const Projects = [
+  {
+    title: "Juego Tic-Tac-Toe",
+    description: "Un mini juego construido con React",
+    url: "https://tic-tac-toe-vite-javierpineyro.vercel.app/"
+  }
+]
