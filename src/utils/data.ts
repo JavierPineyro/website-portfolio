730f47ddf24715bd28df74686e76040459257fa3
JavierPineyro <javidunam22@gmail.com>
2024-05-14 11:20:02 -0300

    Change projects and add types

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,5 @@
+import type { Project } from "./types"
+
 export const Links = [
   {
     label: 'Inicio',
@@ -16,64 +18,6 @@ export const Links = [
     url: '#sobremi',
   },
 ]
-
-// export const Projects = [
-//   {
-//     title: "Juego Tic-Tac-Toe",
-//     description: "Mini juego construído con React y Tailwind",
-//     url: "https://tic-tac-toe-vite-javierpineyro.vercel.app/",
-//     repo: "https://github.com/JavierPineyro/tic-tac-toe-vite",
-//     stack: [
-//       {name: "React", color: "text-pink-400 border-pink-400"}, 
-//       {name: "Tailwind", color: "text-cyan-300 border-cyan-300"}, 
-//       {name: "Vite", color: "text-orange-500 border-orange-500"}
-//     ]
-//   },
-//   {
-//     title: "Search Country App",
-//     description: "Web app para buscar información sobre países",
-//     url: "https://search-country-javierpineyro.vercel.app/",
-//     repo: "https://github.com/JavierPineyro/next-country",
-//     stack: [
-//       {name: "NextJs", color:"text-indigo-400 border-indigo-400"}, 
-//       {name: "React", color: "text-pink-400 border-pink-400"}, 
-//       {name: "Typescript", color:"text-sky-500 border-sky-500"}
-//     ]
-//   },
-//   {
-//     title: "Advency challenge",
-//     description: "Code challenge de navidad de GoncyPozzo",
-//     url: "https://javierpineyro.github.io/advency-challenge/",
-//     repo: "https://github.com/JavierPineyro/advency-challenge",
-//     stack: [
-//       {name: "React", color: "text-pink-400 border-pink-400"}, 
-//       {name: "ChakraUI", color:"text-lime-500 border-lime-500"}, 
-//       {name: "Javascript", color:"text-yellow-500 border-yellow-500"}
-//     ]
-//   },
-//   {
-//     title: "TODO app",
-//     description: "Aplicación Web TO-DO para organizar tareas",
-//     url: "https://javierpineyro.github.io/to-do-chakra/",
-//     repo: "https://github.com/JavierPineyro/to-do-chakra",
-//     stack: [
-//       {name: "React", color: "text-pink-400 border-pink-400"}, 
-//       {name: "ChakraUI", color:"text-lime-500 border-lime-500"}, 
-//       {name: "Vite", color: "text-orange-500 border-orange-500"}
-//     ]
-//   },
-//   {
-//     title: "Mini Tienda w/ Shopping Cart",
-//     description: "Mini Ecommerce con carrito de compras",
-//     url: "https://github.com/JavierPineyro/mini-cart",
-//     repo: "https://github.com/JavierPineyro/mini-cart",
-//     stack: [
-//       {name: "React", color: "text-pink-400 border-pink-400"}, 
-//       {name: "Typescript", color:"text-sky-500 border-sky-500"}, 
-//       {name: "Tailwind", color: "text-cyan-300 border-cyan-300"}
-//     ]
-//   }
-// ]
 
 const tech = {
   react: {
@@ -141,13 +85,13 @@ const tech = {
   }
 }
 
-export const Projects = [
+export const ProjectsList: Project[] = [
   {
     title: "Aplicación de Gestión Turnos",
     description: "App para la gestión turnos para la peluquería/barbería 'Club de Caballeros'",
     thumnail: "https://res.cloudinary.com/javipineyro/video/upload/c_scale,q_60,w_1051/v1715355878/peluqueria_yxxryv.webm",
     
-    url: "https://club-de-caballeros.vercel.app",
+    url: "#top",
     repo: "https://github.com/JavierPineyro/peluqueria-turnos",
     stack: [
       {
@@ -227,48 +171,6 @@ export const Projects = [
       },
     ]
   },
-  // {
-  //   title: "Juego Tic-Tac-Toe",
-  //   description: "Mini juego construído con React y Tailwind",
-  //   thumnail: "https://res.cloudinary.com/javipineyro/video/upload/c_scale,q_60,w_1051/v1684022413/tictactoe-video_xtyplo.webm",
-  //   url: "https://tic-tac-toe-vite-javierpineyro.vercel.app/",
-  //   repo: "https://github.com/JavierPineyro/tic-tac-toe-vite",
-  //   stack: [
-  //     {
-  //       name: tech.react.name,
-  //       color: `${tech.react.colors.text} ${tech.react.colors.border}`
-  //     },
-  //     {
-  //       name: tech.tailwind.name,
-  //       color: `${tech.tailwind.colors.text} ${tech.tailwind.colors.border}`
-  //     },
-  //     {
-  //       name: tech.vite.name,
-  //       color: `${tech.vite.colors.text} ${tech.vite.colors.border}`
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: "TODO app",
-  //   description: "Aplicación Web TO-DO para organizar tareas",
-  //   thumnail: "https://res.cloudinary.com/javipineyro/video/upload/c_scale,q_60,w_1051/v1684278943/todo-chakra-video_kq31fo.webm",
-  //   url: "https://javierpineyro.github.io/to-do-chakra/",
-  //   repo: "https://github.com/JavierPineyro/to-do-chakra",
-  //   stack: [
-  //     {
-  //       name: tech.react.name,
-  //       color: `${tech.react.colors.text} ${tech.react.colors.border}`
-  //     },
-  //     {
-  //       name: tech.chakraui.name,
-  //       color: `${tech.chakraui.colors.text} ${tech.chakraui.colors.border}`
-  //     },
-  //     {
-  //       name: tech.vite.name,
-  //       color: `${tech.vite.colors.text} ${tech.vite.colors.border}`
-  //     }
-  //   ]
-  // },
   {
     title: "Advency challenge",
     description: "Code challenge de navidad de GoncyPozzo",
---
+++ src/utils/types.d.ts
@@ -0,0 +1,12 @@
+export type Project = {
+  title: string;
+  description: string;
+  thumnail: string;
+  url: string;
+  repo: string;
+  stack: {
+      name: string;
+      color: string;
+  }[]
+}
+
